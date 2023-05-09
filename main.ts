e0204cfbc297a20266b1a8bd76dea02e4c551e1d
addyl84 <115687791+addyl84@users.noreply.github.com>
2023-05-09 10:49:29 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let x = 0
+input.onGesture(Gesture.Shake, function () {
+    x = randint(0, 2)
+    if (x == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (x == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (x == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
